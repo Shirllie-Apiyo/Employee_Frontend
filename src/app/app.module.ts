eed180cec6d4cb80830d48d6e340f920f6d08ec3
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-22 23:31:29 +0300

    logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -49,6 +50,7 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatMenuModule,
     MatDividerModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
